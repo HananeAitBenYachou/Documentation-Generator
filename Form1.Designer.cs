7ecbff256d3a4ad6203491c6ad01198fbd6d78e9
Hanane Ait Ben Yachou <hananeaitbenyachou@gmail.com>
2024-02-07 00:42:01 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DocumentationGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAssemblyPath = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAssemblyPath
+            // 
+            this.txtAssemblyPath.BorderRadius = 22;
+            this.txtAssemblyPath.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtAssemblyPath.DefaultText = "";
+            this.txtAssemblyPath.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtAssemblyPath.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtAssemblyPath.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtAssemblyPath.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtAssemblyPath.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtAssemblyPath.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtAssemblyPath.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtAssemblyPath.Location = new System.Drawing.Point(182, 170);
+            this.txtAssemblyPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAssemblyPath.Multiline = true;
+            this.txtAssemblyPath.Name = "txtAssemblyPath";
+            this.txtAssemblyPath.PasswordChar = '\0';
+            this.txtAssemblyPath.PlaceholderText = "";
+            this.txtAssemblyPath.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtAssemblyPath.SelectedText = "";
+            this.txtAssemblyPath.Size = new System.Drawing.Size(530, 137);
+            this.txtAssemblyPath.TabIndex = 0;
+            this.txtAssemblyPath.Validating += new System.ComponentModel.CancelEventHandler(this.txtAssemblyPath_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(24, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Class Library Path :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(165)))));
+            this.label2.Location = new System.Drawing.Point(168, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(426, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Documentation Generator";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BorderRadius = 22;
+            this.btnGenerate.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnGenerate.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnGenerate.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnGenerate.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnGenerate.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnGenerate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.White;
+            this.btnGenerate.Location = new System.Drawing.Point(524, 332);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(188, 63);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(740, 497);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAssemblyPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Documentation Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtAssemblyPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2GradientButton btnGenerate;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentationGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAssemblyPath_Validating(object sender, CancelEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtAssemblyPath.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtAssemblyPath, 
+                    "Please provide the path of the class library for which you would like to generate the documentation.");
+            }
+
+            else if (!File.Exists(txtAssemblyPath.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtAssemblyPath,
+                    "The path you provided does not exists ! please enter a valid path !");
+            }
+
+            else if(!Regex.IsMatch(txtAssemblyPath.Text, @"\.dll$"))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtAssemblyPath,
+                    "The provided path does not point to a class library (DLL) file. Please make sure to provide a valid path to a class library.");
+            } 
+            
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtAssemblyPath, null);
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateChildren())
+            {
+                MessageBox.Show(@"Please provide the path of the class library for which you would like to generate the documentation.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _GenerateDocumentation();
+        }
+
+        private void _GenerateDocumentation()
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                clsDocumentationGenerator docsGenerator = new clsDocumentationGenerator(txtAssemblyPath.Text.Trim());
+            
+                if (docsGenerator.GenerateDocumentation(saveFileDialog1.FileName))
+                {
+                    MessageBox.Show("Documentation generated and saved successfully !", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to generate documentation. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
